6a191bcc9d3a1f8372b2e989fd2680da31e5a890
nus-will <will@nustechnology.com>
2023-09-13 08:23:51 +0700

    update query graphql: remove unuse query

--- src/graphql/types.ts
+++ src/graphql/types.ts
@@ -1,19 +1,16 @@
 export const typeDefs = `
-  type User {
-    email: String!
-    name: String
-  }
   type Speciality {
     name: String!
   }
+
   type Contractor {
     full_name: String!
     day_rate: Int!
     available: Boolean!
     specialities: [Speciality!]
   }
+
   type Query {
-    allUsers: [User!]!
     contractors(_sort: String, _order: String, q: String): [Contractor!]!
   }
 `
